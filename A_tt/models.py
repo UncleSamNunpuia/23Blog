b15e222efae55bcb0f260525980748ca4add6fb4
UncleSamNunpuia <70100222+UncleSamNunpuia@users.noreply.github.com>
2023-03-10 23:22:24 +0530

    c2

--- A_tt/models.py
+++ A_tt/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+# added below to make file and pics uploadable
+
 
 class MyModel(models.Model):
     my_file = models.FileField(upload_to='uploads/')
